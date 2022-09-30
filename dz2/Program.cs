2bde6b9c2c45fd5c136ba4010583be373298eaf9
Menua <menya2012avah@mail.ru>
2022-09-30 15:59:31 +0300

    2dz

---
+++ dz2/Program.cs
@@ -0,0 +1,26 @@
+﻿ Console.WriteLine("Введите 3 числа ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+    Console.WriteLine($"{a} наибольшее число");
+    }
+    else
+    {
+    Console.WriteLine($"{c} наибольшее число");
+    }
+    }else{
+    if (c > b)
+    {
+    Console.WriteLine($"{c} наибольшее число");
+    }
+     else
+    {
+    Console.WriteLine($"{b} наибольшее число");
+    }
+    }

cdcae1d3ca3e60504fed1aeca08b87044b2aa94c
Menua <menya2012avah@mail.ru>
2022-09-30 13:06:55 +0300

    задание 1

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            Console.WriteLine("Введите 2 числа ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("{0}наибольшее число", a);
+            }
+            else
+            {
+                Console.WriteLine("{0}наибольшее число", b);
+            }
+        }
+    }
+}
